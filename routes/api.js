893cfac8ca18198c4bdf0afc154f007e2a9c5c60
Mike Fallesen <mfallesen2010@gmail.com>
2020-11-01 23:34:07 -0800

    api rotues finished for testing

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,28 @@
 const router = require("express").Router();
-const wod = require("../models/workoutroutine.js");
-const exercise = require("../models/Exercise.js")
+const mongoose = require("mongoose");
+const db = require("../models");
 
-router.get("/api/")
+mongoose.connect("mongodb://localhost/wod", {
+    useNewUrlParser:true,
+    useFindAndModify: false
+});
+
+
+// get all current workout data
+router.get("/api/totalwork", (req, res) => {
+    db.Workout.find({}).then(data => {
+        res.json(data);
+    }).catch(err => {
+        res.json(err)
+    });
+});
+
+// add workout
+
+router.post("/api/newworkout", (req, res) => {
+    db.Workout.create(req.body).then(data => {
+        res.json(data);
+    }).catch(err => {
+        res.json(err)
+    });
+});
